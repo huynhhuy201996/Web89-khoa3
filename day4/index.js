c5b6ce1ca8d73e2fb3a34c38ea0742bf547d21e2
huynhhuy1996 <origamizs8588244@gmail.com>
2024-01-20 14:06:01 +0700

    mongodb

--- day4/index.js
+++ day4/index.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { query } from "express";
 import mongoose from "mongoose";
 
 const server = express();
@@ -7,11 +7,14 @@ const server = express();
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 
+server.use(express.json());
+
 const BlogPost = new Schema({
     author: String, //objectID
     title: String,
     body: String,
-    date: Date
+    date: String,
+
 });
 
 const BlogModel = mongoose.model("blog", BlogPost);
@@ -25,27 +28,65 @@ server.get('/', (req, res) => {
     res.status(200).send('hello world!')
 })
 
-server.get("/blogs", async (req, res) => {
-    const blogs = await BlogModel.find({});
-    res.status(200).send(blogs)
-})
+
 
 // Thêm bài viết vào data 
 server.post("/blogs", async (req, res) => {
     try {
-        const Newblogs = await BlogModel.create({
-            author: "naruto",
-            title: "hello !",
-            body: " hello every one !!!",
-            date: new Date()
-        })
-        res.status(200).send(Newblogs)
+        // const newBlog = new BlogModel({
+        //   author: "Nam",
+        //   title: "Hello",
+        //   body: "Hello world from Mindx!",
+        //   date: new Date(),
+        // });
+
+        // await newBlog.save();
+
+        const body = req.body;
+        // Validate input
+        if (!body.author) throw new Error("Author field is required!");
+        if (!body.title) throw new Error("Title field is required!");
+        if (!body.body) throw new Error("Body field is required!");
+        if (!body.date) throw new Error("Date field is required!");
+
+        const newBlog = await BlogModel.create({
+            author: body.author,
+            title: body.title,
+            body: body.body,
+            date: body.date,
+        });
+
+        res.status(201).send(newBlog);
     } catch (error) {
         console.log(error);
-        res.status(500).send(" server error !!!")
+        res.status(500).send(error.message);
     }
+});
 
-})
+// lấy tất cả trừ id bằng query params - lấy thông tin chung của toàn bộ database
+server.get("/blogs", async (req, res) => {
+    try {
+        const query = req.query;
+        const blogs = await BlogModel.find(query);
+        res.status(200).send(blogs);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('server error!!!')
+    }
+});
+
+//get Id lấy mỗi ID_ lấy detail của 1 mặt hàng duy nhất
+server.get("/blogs/:blogId", async (req, res) => {
+    try {
+        const blogId = req.params.blogId;
+        const blog = await BlogModel.findById(blogId);
+        res.status(200).send(blog);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('server error !!!');
+
+    };
+});
 
 connectDb().then(
     server.listen(3000, () => {
